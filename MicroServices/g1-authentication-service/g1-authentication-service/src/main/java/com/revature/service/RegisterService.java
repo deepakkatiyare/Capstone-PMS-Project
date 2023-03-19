a9829be8a2b2c1e5f9fa199d8070db10384d1a0f
Deepak <61901541+deepakkatiyare@users.noreply.github.com>
2023-03-20 00:02:57 +0530

    Delete MicroServices directory

--- MicroServices/g1-authentication-service/g1-authentication-service/src/main/java/com/revature/service/RegisterService.java
+++
@@ -1,9 +0,0 @@
-package com.revature.service;
-
-import org.springframework.stereotype.Service;
-
-import com.revature.entity.dto.PatientDto;
-
-public interface RegisterService {
-	public PatientDto registerService(PatientDto patientDto);
-}
