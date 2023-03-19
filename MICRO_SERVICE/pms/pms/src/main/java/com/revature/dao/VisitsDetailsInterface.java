3130d4521a304eda77ea63b065b88afe1ee96470
Deepakkatiyare <amankukreti583@gmail.com>
2023-03-20 00:53:18 +0530

    patient module

--- MICRO_SERVICE/pms/pms/src/main/java/com/revature/dao/VisitsDetailsInterface.java
+++
@@ -1,9 +0,0 @@
-package com.revature.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revature.model.VisitDetails;
-
-public interface VisitsDetailsInterface extends JpaRepository<VisitDetails, Integer>{
-
-}
